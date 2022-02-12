0a722a15d20e81decb8d43233bbf0b33e3995adb
conor <doyle.conorp@gmail.com>
2022-02-12 13:21:38 -0800

    Add pseudocode and head title

---
+++ scripts.js
@@ -0,0 +1,8 @@
+/* PSEUDOCODE
+Create function computerPlay that randomly returns 'Rock', 'Paper', or 'Scissors'
+Create function game().
+Create & call playRound function inside game() which keeps score and reports winner
+Console.log() results of each round and winner at the end
+Use prompt() to get input from user
+*/
+
